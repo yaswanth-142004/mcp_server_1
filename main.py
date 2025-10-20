b51c3089678a6a8292308f9c1bee4b6f52f468a7
Yaswanth V <yashwanth142004@gmail.com>
2025-10-20 21:52:19 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastmcp import FastMCP 
+
+
+mcp = FastMCP("Personal Server")
+
+@mcp.tool
+def get_my_details(secret_code:str):
+    return {
+        "name":"yaswanth",
+        "age":20,
+        "work":"SDE-AI",
+        "about":"Can build sophisticated agents along with a amazing web applications",
+        "hobbies":"can cook your soul soothing food , can code for 20 hrs a day and can listen to all your bullshit."
+        
+        
+    }
+    
+    
+    
+    if __name__ == "__main__":
+        mcp.run(transport="http",port=1234)
+    
+    
+    
